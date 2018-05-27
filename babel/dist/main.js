0423f9470ba500148ebb2882c861f6524555f4ab
rain-js <unique_ritchie@163.com>
2018-05-27 17:52:03 +0800

    :apple:use babel

---
+++ babel/dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var i = 0;
+
+setInterval(function () {
+    console.log("hello babel " + i++);
+}, 1000);
---
+++ babel/src/main.js
@@ -0,0 +1,5 @@
+let i = 0
+
+setInterval(() => {
+    console.log(`hello babel ${i++}`)
+}, 1000)
